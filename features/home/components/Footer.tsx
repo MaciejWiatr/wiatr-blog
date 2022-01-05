1dd962a430da85982c9ff41a905c2502caaf7d01
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-05 12:58:13 +0100

    Fix footer styling

--- features/home/components/Footer.tsx
+++ features/home/components/Footer.tsx
@@ -2,17 +2,17 @@ import Twemoji from "@shared/components/Twemoji";
 
 const Footer = () => {
 	return (
-		<footer className="border-t border-gray-600 p-8 flex flex-wrap">
+		<footer className="border-t border-gray-600 pt-8 pb-8 flex flex-wrap">
 			<div className="w-full md:w-1/2">
 				<h2 className="text-xl">Może zostaniesz na dłużej?</h2>
-				<p className="text-gray-400 max-w-md text-sm">
+				<p className="text-gray-400 max-w-md md:text-sm text-xs">
 					Zapisz się do mojego newslettera aby regularnie otrzymywać
 					informacje o nowych postach
 				</p>
 				<form className="mt-4 space-x-2 flex">
 					<input
 						placeholder="Twój adres email"
-						className="bg-transparent border border-gray-400 rounded p-2 h-12 placeholder:text-sm"
+						className="bg-transparent border border-gray-400 rounded p-2 h-12 placeholder:text-sm w-full max-w-xs"
 					/>
 					<button className="bg-white text-gray-800 h-12 rounded p-2 pl-4 pr-4 flex items-center">
 						<span className="font-medium text-sm mr-1">Dołącz</span>{" "}
@@ -22,7 +22,7 @@ const Footer = () => {
 			</div>
 			<div className="mt-4 md:mt-0 w-full md:w-1/2 md:text-right">
 				<h2 className="text-xl">Kontakt</h2>
-				<span className="text-gray-400 max-w-md text-sm">
+				<span className="text-gray-400 max-w-md md:text-sm text-xs">
 					email:{" "}
 					<span className="font-semibold">
 						maciej.wiatr00@gmail.com
