b5cc80c7a1e413dc4cad93050331dfcbbb40d53b
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-07 12:23:38 +0100

    Add smooth page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
 			<Head>
 				<title>Blog Macieja Wiatra</title>
 			</Head>
-			<Component {...pageProps} />
+			<SwitchTransition mode="out-in">
+				<CSSTransition
+					key={router.pathname}
+					classNames="page"
+					timeout={300}
+				>
+					<Component {...pageProps} />
+				</CSSTransition>
+			</SwitchTransition>
 		</>
 	);
 }
