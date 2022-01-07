8497ef1a7a56864cd6ab75da1d169a14e6aa08aa
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-07 11:53:36 +0100

    Remove unused code

--- features/article/pages/ArticlePage.tsx
+++ features/article/pages/ArticlePage.tsx
@@ -1,30 +1,5 @@
 import Image from "next/image";
 import ArticleLayout from "../layouts/ArticleLayout";
-import ReactMarkdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
-
-const mockArticle = {
-	data: {
-		article: {
-			title: "Co tam  w kinie słychać",
-			tags: [
-				{
-					name: "TypeScript",
-				},
-			],
-			slug: "co-tam-w-kinie-slychac",
-			image: {
-				url: "https://media.graphcms.com/87163A3TvePx7H3GZB0g",
-			},
-			content: {
-				markdown:
-					'# To jest tytuł kategorii\n\n> mega fajne to kox\n\n    a to nie, to juz jest zwykły tekst\n\n<iframe width="300" height="250" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="https://www.youtube.com/embed/860d8usGC0o" src="https://www.youtube.com/embed/860d8usGC0o"></iframe>\n\nokej nawet nie najgorzej się w tym pisze\n\n**zastanawiam się tylko jak dobrze będzie mi się w tym pisało na dłuższą mete**\n\n****\n\n| tabelka | tabelkowa |\n| ------- | --------- |\n| test    | testowo   |\n\n****\n',
-			},
-			createdAt: "2022-01-05T03:32:29.747696+00:00",
-		},
-	},
-};
 
 const ArticlePage = ({ article }) => {
 	return (
