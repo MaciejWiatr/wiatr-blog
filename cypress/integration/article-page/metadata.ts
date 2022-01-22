d1ecc8fa4614301707259178f1e938ec2986d4b0
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-22 15:26:31 +0100

    Add article page tests and corresponding cypress data attrs

---
+++ cypress/integration/article-page/metadata.ts
@@ -0,0 +1,22 @@
+describe("Page contains correct metadata", () => {
+	beforeEach(() => {
+		cy.visit("/article/jakich-technologii-uzylem-do-budowy-bloga-w-2021");
+	});
+	it("should have correct title", () => {
+		cy.window()
+			.title()
+			.should(
+				"contain",
+				"Jakich technologii użyłem do budowy bloga w 2021"
+			);
+	});
+	it("should have correct metadata", () => {
+		cy.document()
+			.get("head meta[property='og:title']")
+			.should(
+				"have.attr",
+				"content",
+				"Jakich technologii użyłem do budowy bloga w 2021"
+			);
+	});
+});
---
+++ cypress/integration/article-page/reactions.ts
@@ -0,0 +1,15 @@
+describe("Reactions should work properly", () => {
+	beforeEach(() => {
+		cy.visit("/article/jakich-technologii-uzylem-do-budowy-bloga-w-2021");
+	});
+	it("Should send correct request on reaction", () => {
+		cy.intercept("/api/article/reaction", {
+			message: "Reaction created",
+		}).as("reactionPost");
+		cy.get("[data-cy=reaction-Super]").click();
+		cy.wait("@reactionPost").its("request.body").should("deep.include", {
+			reactionName: "Super",
+			articleId: "ckyok8sg0174w0c995mvrz2wq",
+		});
+	});
+});
