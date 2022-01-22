6c079d98dbd44e7561cee826f63a9f36223760be
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-22 11:44:59 +0100

    Split cypress tests to smaller chunks

---
+++ cypress/integration/home-page/tag_search.ts
@@ -0,0 +1,15 @@
+describe("Tag search work properly", () => {
+	it("Should hide post if it doesnt include searched tag", () => {
+		cy.visit("/");
+		cy.get("[data-cy=tag-SQL]").click();
+		cy.get("[data-cy=article-link-ckyok8sg0174w0c995mvrz2wq]").should(
+			"not.be.visible"
+		);
+	});
+	it("Should show post if it does include searched tag", () => {
+		cy.get("[data-cy=tag-Blogowanie]").click();
+		cy.get("[data-cy=article-link-ckyok8sg0174w0c995mvrz2wq]").should(
+			"be.visible"
+		);
+	});
+});
