f35cb9b908262638dd3f2c80eb27f85c222554c3
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-27 14:52:17 +0100

    Fix cypress tests

--- cypress/integration/home-page/redirects.ts
+++ cypress/integration/home-page/redirects.ts
@@ -1,7 +1,7 @@
 describe("Redirects work properly", () => {
 	it("should correctly redirect to article page and go back", () => {
 		cy.visit("/");
-		cy.get("[data-cy*=article-link]").click();
+		cy.get("[data-cy=article-link-ckyok8sg0174w0c995mvrz2wq]").click();
 		cy.url().should("include", "/article");
 		cy.get("[data-cy=home-link]").click();
 		cy.url().should("include", "/");
