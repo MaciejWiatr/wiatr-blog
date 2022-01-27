de33abd1ddcecea834ca18ca76a54ee7cef7b218
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-27 14:46:28 +0100

    Improve backend request validation

--- features/home/utils/filterArticles.ts
+++ features/home/utils/filterArticles.ts
@@ -1,4 +1,6 @@
-const filterArticlesByTagName = (articles, filter) => {
+import { Article } from "@shared/graphql/generated";
+
+const filterArticlesByTagName = (articles: Article[], filter: string) => {
 	return articles.filter((article) =>
 		article.tags.some((tag) => tag.name.includes(filter))
 	);
